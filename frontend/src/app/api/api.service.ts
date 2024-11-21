4a1491badad6cd976db960d49f474fc3d66002f5
RafVanhoegaerden-s128965 <s128965@ap.be>
2024-11-21 15:15:06 +0100

    Backend: fix issue + add GET/POST calls

--- frontend/src/app/api/api.service.ts
+++ frontend/src/app/api/api.service.ts
@@ -6,35 +6,30 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  public apiUrl = 'http://127.0.0.1:8000/api/submit_text'; // Update als je backend anders draait
+  public baseUrl = 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) {}
 
-  // Functie om de tekst naar de backend te sturen
-  submitText(formData: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, formData);
-  }
-
-  // AI: Verkrijg suggesties (POST /api/get_suggestions)
-  getSuggestions(text: string, text_type: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/get_suggestions`, {
-      text,
-      text_type,
+  // POST raw-text
+  postRawText(input_text: string, input_type: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/llm/raw_text`, {
+      input_text,
+      input_type,
     });
   }
 
-  // Scraper: Verkrijg lijst van items (GET /api/items)
-  getItems(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/items`);
-  }
+  // Verkrijg lijst van items (GET /api/items)
+  // getItems(): Observable<any> {
+  //   return this.http.get<any>(`${this.apiUrl}/items`);
+  // }
 
-  // Scraper: Verkrijg een specifiek item op basis van item_id (GET /api/items/{item_id})
-  getItemById(itemId: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/items/${itemId}`);
-  }
+  // Verkrijg een specifiek item op basis van item_id (GET /api/items/{item_id})
+  // getItemById(itemId: string): Observable<any> {
+  //   return this.http.get<any>(`${this.apiUrl}/items/${itemId}`);
+  // }
 
-  // Scraper: Maak een nieuw item aan (POST /api/items/{item_id})
-  createItem(itemId: string, itemData: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/items/${itemId}`, itemData);
-  }
+  // Maak een nieuw item aan (POST /api/items/{item_id})
+  // createItem(itemId: string, itemData: any): Observable<any> {
+  //   return this.http.post<any>(`${this.apiUrl}/items/${itemId}`, itemData);
+  // }
 }
